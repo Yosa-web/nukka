d405f97343f3629eaf7f3ee4b4efb641d2460ac3
MAbdiFirdaus <abdidaus12@gmail.com>
2024-10-01 11:09:46 +0700

    penambahan controller dan model

---
+++ app/Controllers/OperatorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class OperatorController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
